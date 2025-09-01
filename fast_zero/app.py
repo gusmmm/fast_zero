29807e4c53975c44b3e9447fe48646a09d266a77
gusmmm <jgustavomartins@gmail.com>
2025-09-01 22:38:49 +0100

    Adicionei schema de mensagem

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,14 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import MessageResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/",
+         status_code=HTTPStatus.OK,
+         response_model=MessageResponse)
 def read_root():
     return {'message': 'Ola Mundo!'}
